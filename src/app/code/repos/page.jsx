18153f12412576f1481ce964a3b51af1abd85935
Dalzon Charles-Hébert <charlby5@gmail.com>
2023-04-20 14:36:20 -0400

    Create the Repos page

---
+++ src/app/code/repos/page.jsx
@@ -0,0 +1,41 @@
+import Link from 'next/link';
+import { FaStar, FaCodeBranch, FaEye } from 'react-icons/fa';
+
+async function getRepos() {
+  const response = await fetch('https://api.github.com/users/tichif/repos');
+  const repos = await response.json();
+  return repos;
+}
+
+const ReposPage = async () => {
+  const repos = await getRepos();
+
+  return (
+    <div className='repos-container'>
+      <h2>Repositories</h2>
+      <ul className='repo-list'>
+        {repos.map((repo) => (
+          <li key={repo.id}>
+            <Link href={`/code/repos/${repo.name}`}>
+              <h3>{repo.name}</h3>
+              <p>{repo.description}</p>
+              <div className='repo-details'>
+                <span>
+                  <FaStar /> {repo.stargazers_count}
+                </span>
+                <span>
+                  <FaCodeBranch /> {repo.forks_count}
+                </span>
+                <span>
+                  <FaEye /> {repo.watchers_count}
+                </span>
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ReposPage;
